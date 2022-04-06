a5708616ea0f804a827c9396bb307d91ed9012dd
DokySp <uhug@naver.com>
2022-04-06 22:48:09 +0900

    테마 파일 일부 수정 / 불필요한 파일 삭제

--- lib/util/theme/colors.dart
+++ lib/util/theme/colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class DDColor {
   static final MaterialColor primary = MaterialColor(
-    0xFFEF6560,
+    0xFFEF5350,
     <int, Color>{
       400: Color(Colors.red.shade800.value),
       500: const Color(0xFFEF5350),
@@ -15,6 +15,9 @@ class DDColor {
   static const Color widgetBackgroud = Colors.white;
   static const Color background = Color(0xFFF5F5F5);
   static const Color white = Colors.white;
+  static final Color grey = Colors.grey.shade400;
+
+  static final Color fontColor = Colors.grey.shade900;
 
   // 색상 조율
   // static const Color white = Colors.grey.shade900;
---
+++ lib/util/theme/font.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+/// - H1: 45pt
+/// - H2: 23pt
+/// - H3: 20pt
+/// - H4: 15pt
+/// - H5: 13.5pt
+/// - H6: 12pt
+class DDFontSize {
+  static const double h1 = 45;
+  static const double h15 = 27;
+  static const double h2 = 23;
+  static const double h3 = 20;
+  static const double h35 = 18;
+  static const double h4 = 15;
+  static const double h5 = 13.5;
+  static const double h6 = 12;
+}
+
+// 12
+// 13
+// 13.5
+
+// 14
+// 15
+// 16
+
+// 20
+// 22
+// 23
+
+// 24
+// 30
+
+// 45
+
+class DDFontWeight {
+  static const light = FontWeight.w300;
+  static const regular = FontWeight.w500;
+  static const bold = FontWeight.w700;
+  static const extraBold = FontWeight.w900;
+}
+
+class DDFontFamily {
+  static const nanumSR = "NanumSR";
+}
