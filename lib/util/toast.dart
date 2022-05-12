72941c3ac3f2453c41c5c84b752daf8c954fa7e1
DokySp <uhug@naver.com>
2022-05-12 20:41:43 +0900

    Toast 진동 기능 추가

--- lib/util/toast.dart
+++ lib/util/toast.dart
@@ -1,9 +1,11 @@
 import 'package:app/util/theme/font.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:vibration/vibration.dart';
 
 class DDToast {
   static void showToast(String msg) {
+    _vibration();
     Fluttertoast.showToast(
       msg: msg,
       toastLength: Toast.LENGTH_SHORT,
@@ -12,5 +14,15 @@ class DDToast {
       textColor: Colors.white,
       fontSize: DDFontSize.h4,
     );
+  }
+
+  static Future<void> _vibration() async {
+    await Vibration.vibrate(
+        pattern: [0, 40, 15, 40, 120, 70], intensities: [220, 150, 255]);
+
+    // await Future.delayed(const Duration(milliseconds: 1000));
+    // await Vibration.vibrate(duration: 80);
+    // await Future.delayed(const Duration(milliseconds: 200));
+    // await Vibration.vibrate(duration: 200);
   }
 }
